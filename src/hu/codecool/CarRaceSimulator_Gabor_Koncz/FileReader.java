8ac87045da7d402c43aa51d74a610d29e2301556
gabee1987 <gabee1987@gmail.com>
2017-08-15 15:12:52 +0200

    Created a FileReader module

--- src/hu/codecool/CarRaceSimulator_Gabor_Koncz/FileReader.java
+++ src/hu/codecool/CarRaceSimulator_Gabor_Koncz/FileReader.java
@@ -1,4 +1,23 @@
 package hu.codecool.CarRaceSimulator_Gabor_Koncz;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileReader {
+    public static List readFile(String fileName) {
+        List<String> result = new ArrayList<>();
+        try {
+            Scanner data = new Scanner(new File(fileName));
+            while (data.hasNextLine()) {
+                result.add(data.nextLine());
+            }
+        } catch (FileNotFoundException) {
+            System.err.println("File not found.");
+        }
+        return result;
+    }
 }
+
